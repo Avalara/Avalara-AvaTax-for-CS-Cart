23d863a35006aa2d714e0af79c8845bc7708a5e4
Vijay Nalawade <vijay.nalawade@avalara.com>
2020-02-19 12:54:37 +0530

    Update avatax_create_account.php
    Removed hardcoded credentials and commented code

--- Avalara AvaTax for CS Cart version 00.00.11.00/design/backend/app/addons/avatax_tax_calculation/lib/AvaTax4PHP/avatax_create_account.php
+++ Avalara AvaTax for CS Cart version 00.00.11.00/design/backend/app/addons/avatax_tax_calculation/lib/AvaTax4PHP/avatax_create_account.php
@@ -61,11 +61,6 @@ $json='{
 	}
 }';
 
-
-//$jsonResponse='{"Message":"Account created successfully!\u000a\u000dUser created successfully!\u000a\u000dCompany created successfully!\u000a\u000dLocation created successfully!\u000a\u000dNexus created successfully!\u000a\u000d","Result":{"__type":"AccountSubscription:#AvaTaxSelfProcLib.Models","AccountId":"2000002537","CampaignId":"Test","Company":{"BIN":"undefined","CompanyAddr":{"City":"BainBridge Island","Country":"US","Line1":"900 winslow way E","Line2":"","Line3":"","State":"WA","Zip":"98110-2450"},"CompanyCode":"default","CompanyContact":{"Email":"chaitanya.chapekar@avalara.com","Fax":"","FirstName":"cc","LastName":"testcc","MobileNumber":"","PhoneNumber":"1234567890","Title":"MR"},"CompanyName":"testcc","TIN":"111111111"},"ConnectorName":"CS Cart","EffDate":"2015-11-09","EndDate":"2015-12-09","LeadSourceMostRecent":"Test","LicenseKey":"7CA013D5F077E414","PaymentMethodId":"","ProductRatePlans":["ZT - Free - Pro"],"User":{"TempPwd":"wcWC81-!","UserName":"chaitanya.chapekar@avalara.com"}},"Status":"Success"}';
-
-//print_r($jsonResponse);
-//exit;
 $url = 'https://onboarding.api.avalara.com/v1/Accounts';
 
 $authentication = base64_encode(ONBOARDING_USERNAME.":".ONBOARDING_PASSWORD);
@@ -99,19 +94,4 @@ $curl_error = curl_error($ch);
 curl_close($ch);
 //echo "<p>CURL Response</p>";
 print_r($data);
-
-
-/*$data = '{"Message":"Account created successfully!\u000a\u000dUser created successfully!\u000a\u000dCompany created successfully!\u000a\u000dLocation created successfully!\u000a\u000dNexus created successfully!\u000a\u000d","Result":{"__type":"AccountSubscription:#AvaTaxSelfProcLib.Models","AccountId":"2000000334","CampaignId":"Test","Company":{"BIN":"","CompanyAddr":{"City":"Seattle","Country":"US","Line1":"ADDRESS LINE 2","Line2":"1000 2nd Ave","Line3":"","State":"WA","Zip":"98104-1094"},"CompanyCode":"default","CompanyContact":{"Email":"vijay@vijay1.com","Fax":"","FirstName":"My Name","LastName":"N","MobileNumber":"","PhoneNumber":"123","Title":""},"CompanyName":"My Store","TIN":"123456789"},"ConnectorName":"QuickBooks Online","EffDate":"2015-08-25","EndDate":"2015-09-24","LeadSourceMostRecent":"Test","LicenseKey":"ADECA7FB01A67F94","ProductRatePlans":["ZT - Free - Pro"],"User":{"TempPwd":"ydYD91-!","UserName":"vijay@vijay1.com"}},"Status":"Success"}';*/
-
-//print_r($data);
-/*$json = json_decode($data);
-
-if($json->Status=="Error")
-{
-	echo "Error in Creating Account - ".$json->Message;
-}*/
-
-//Invalid response
-//CURL Response</p>{"Message":"ConnectorName could not be found!","Result":"Validation error","Status":"Error"} - If we send OpenCart in connector name
-//CURL Response</p>{"Message":"Account created successfully!\u000a\u000dUser created successfully!\u000a\u000dCompany created successfully!\u000a\u000dLocation created successfully!\u000a\u000dNexus created successfully!\u000a\u000d","Result":{"__type":"AccountSubscription:#AvaTaxSelfProcLib.Models","AccountId":"2000000185","CampaignId":"Test","Company":{"BIN":"124","CompanyAddr":{"City":"Seattle","Country":"US","Line1":"1000 2nd Ave","Line2":"","Line3":"","State":"WA","Zip":"98104-1094"},"CompanyCode":"default","CompanyContact":{"Email":"vijay@vijay.com","Fax":"","FirstName":"Vijay","LastName":"Nalawade","MobileNumber":"","PhoneNumber":"7276888868","Title":"Mr"},"CompanyName":"Vijay Store","TIN":"123456789"},"ConnectorName":"QuickBooks Online","EffDate":"2015-08-13","EndDate":"2015-09-12","LeadSourceMostRecent":"Test","LicenseKey":"E7772D30BD1DBBB5","ProductRatePlans":["ZT - Free - Pro"],"User":{"TempPwd":"viVJ83_$","UserName":"vijay@vijay.com"}},"Status":"Success"}
 ?>
