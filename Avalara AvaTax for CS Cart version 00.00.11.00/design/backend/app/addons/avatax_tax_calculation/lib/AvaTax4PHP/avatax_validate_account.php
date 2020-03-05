b027511f4721d5c5453476488c2b0af5ea76820d
Vijay Nalawade <vijay.nalawade@avalara.com>
2020-03-05 12:30:01 +0530

    Removed hard-coded credentials
    Removed hardcoded credentials from the comment lines

--- Avalara AvaTax for CS Cart version 00.00.11.00/design/backend/app/addons/avatax_tax_calculation/lib/AvaTax4PHP/avatax_validate_account.php
+++ Avalara AvaTax for CS Cart version 00.00.11.00/design/backend/app/addons/avatax_tax_calculation/lib/AvaTax4PHP/avatax_validate_account.php
@@ -6,7 +6,6 @@ $password=$_REQUEST['password'];
 $url = 'https://sandbox.onboarding.api.avalara.com/v1/Accounts/'.$accountId;
 
 $authentication=base64_encode($username.":".$password);
-//$authentication = base64_encode("TEST/vijay.nalawade@avalara.com:Avalara@123");
 
 $ch = curl_init($url);
 $options = array(
@@ -32,9 +31,6 @@ curl_close($ch);
 //echo "<p>CURL Response</p>";
 print_r($data);
 
-
-/*$data = '{"Message":"Account created successfully!\u000a\u000dUser created successfully!\u000a\u000dCompany created successfully!\u000a\u000dLocation created successfully!\u000a\u000dNexus created successfully!\u000a\u000d","Result":{"__type":"AccountSubscription:#AvaTaxSelfProcLib.Models","AccountId":"2000000334","CampaignId":"Test","Company":{"BIN":"","CompanyAddr":{"City":"Seattle","Country":"US","Line1":"ADDRESS LINE 2","Line2":"1000 2nd Ave","Line3":"","State":"WA","Zip":"98104-1094"},"CompanyCode":"default","CompanyContact":{"Email":"vijay@vijay1.com","Fax":"","FirstName":"My Name","LastName":"N","MobileNumber":"","PhoneNumber":"123","Title":""},"CompanyName":"My Store","TIN":"123456789"},"ConnectorName":"QuickBooks Online","EffDate":"2015-08-25","EndDate":"2015-09-24","LeadSourceMostRecent":"Test","LicenseKey":"ADECA7FB01A67F94","ProductRatePlans":["ZT - Free - Pro"],"User":{"TempPwd":"ydYD91-!","UserName":"vijay@vijay1.com"}},"Status":"Success"}';*/
-
 //print_r($data);
 /*$json = json_decode($data);
 
@@ -43,7 +39,4 @@ if($json->Status=="Error")
 	echo "Error in Creating Account - ".$json->Message;
 }*/
 
-//Invalid response
-//CURL Response</p>{"Message":"ConnectorName could not be found!","Result":"Validation error","Status":"Error"} - If we send OpenCart in connector name
-//CURL Response</p>{"Message":"Account created successfully!\u000a\u000dUser created successfully!\u000a\u000dCompany created successfully!\u000a\u000dLocation created successfully!\u000a\u000dNexus created successfully!\u000a\u000d","Result":{"__type":"AccountSubscription:#AvaTaxSelfProcLib.Models","AccountId":"2000000185","CampaignId":"Test","Company":{"BIN":"124","CompanyAddr":{"City":"Seattle","Country":"US","Line1":"1000 2nd Ave","Line2":"","Line3":"","State":"WA","Zip":"98104-1094"},"CompanyCode":"default","CompanyContact":{"Email":"vijay@vijay.com","Fax":"","FirstName":"Vijay","LastName":"Nalawade","MobileNumber":"","PhoneNumber":"7276888868","Title":"Mr"},"CompanyName":"Vijay Store","TIN":"123456789"},"ConnectorName":"QuickBooks Online","EffDate":"2015-08-13","EndDate":"2015-09-12","LeadSourceMostRecent":"Test","LicenseKey":"E7772D30BD1DBBB5","ProductRatePlans":["ZT - Free - Pro"],"User":{"TempPwd":"viVJ83_$","UserName":"vijay@vijay.com"}},"Status":"Success"}
 ?>
